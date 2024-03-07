f1d258d5346619acbab1070e15fd486576e493e0
Loris Leiva <loris.leiva@gmail.com>
2024-03-07 09:53:48 +0000

    Update pre-publish script

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -14,8 +14,8 @@ for (const projectName of projects) {
   const projectDirectory = path.resolve(projectsDirectory, projectName);
   cd(projectDirectory);
 
+  // Add all changes and commit them.
   await $`git add -A .`;
-
   try {
     await $`git commit -m "version ${version} snapshot"`;
   } catch (e) {
@@ -24,10 +24,14 @@ for (const projectName of projects) {
     }
   }
 
+  // Tag the commit.
   await $`git tag -m "v${version}" v${version}`;
-  await $`git push --follow-tags`;
+
+  // Push the changes on main.
+  await $`git push origin HEAD:main --follow-tags`;
 }
 
+// Update submodules on the root directory.
 cd(rootDirectory);
 await $`git add projects`;
 await $`git commit -m 'Update snapshot' --allow-empty`;

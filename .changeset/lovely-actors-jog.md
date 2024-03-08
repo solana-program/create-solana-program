a7e46b7d060faab7b29d977438a4bce359a7fea4
Loris Leiva <loris.leiva@gmail.com>
2024-03-08 10:38:53 +0000

    Prefix dot files with _. in templates

---
+++ .changeset/lovely-actors-jog.md
@@ -0,0 +1,5 @@
+---
+"create-solana-program": patch
+---
+
+Prefix dot files with \_. in templates
--- utils/renderTemplates.ts
+++ utils/renderTemplates.ts
@@ -16,7 +16,7 @@ import {
  * Renders a template folder/file to the provided destination,
  * by recursively copying all files under the source directory,
  * with the following exception:
- *   - `_filename` are renamed to `.filename`.
+ *   - `_.filename` are renamed to `.filename`.
  *   - `package.json` files are merged.
  *   - `.gitignore` files are concatenated.
  *   - `.njk` files are rendered as nunjucks templates
@@ -40,20 +40,25 @@ export function renderTemplate(ctx: RenderContext, src: string, dest: string) {
 
   const filename = path.basename(src);
 
+  // Rename `_.file` to `.file` in the destination.
+  if (filename.startsWith('_.')) {
+    dest = path.resolve(path.dirname(dest), filename.replace(/^_./, '.'));
+  }
+
+  // Concatenate .gitignore files.
+  if (filename === '_.gitignore' && fs.existsSync(dest)) {
+    const existing = fs.readFileSync(dest, 'utf8');
+    const newGitignore = fs.readFileSync(src, 'utf8');
+    fs.writeFileSync(dest, existing + '\n' + newGitignore);
+    return;
+  }
+
   // Merge package.json files.
   if (filename === 'package.json' && fs.existsSync(dest)) {
     const existing = JSON.parse(fs.readFileSync(dest, 'utf8'));
     const newPackage = JSON.parse(fs.readFileSync(src, 'utf8'));
     const pkg = sortDependencies(deepMerge(existing, newPackage));
     fs.writeFileSync(dest, JSON.stringify(pkg, null, 2) + '\n');
-    return;
-  }
-
-  // Append to existing .gitignore.
-  if (filename === '.gitignore' && fs.existsSync(dest)) {
-    const existing = fs.readFileSync(dest, 'utf8');
-    const newGitignore = fs.readFileSync(src, 'utf8');
-    fs.writeFileSync(dest, existing + '\n' + newGitignore);
     return;
   }
 

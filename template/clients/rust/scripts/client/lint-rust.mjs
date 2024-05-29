3afe0c42d260dae3ddd0a453ba8d02f69418be2b
Loris Leiva <loris.leiva@gmail.com>
2024-05-29 13:43:45 +0100

    Fix formatting

--- template/clients/rust/scripts/client/lint-rust.mjs
+++ template/clients/rust/scripts/client/lint-rust.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
-import "zx/globals";
-import { workingDirectory } from "../utils.mjs";
+import 'zx/globals';
+import { workingDirectory } from '../utils.mjs';
 
 // Check the client using Clippy.
-cd(path.join(workingDirectory, "clients", "rust"));
+cd(path.join(workingDirectory, 'clients', 'rust'));
 await $`cargo clippy ${argv._}`;

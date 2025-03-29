9eaf3a7106c28c8c0210eea299e24de6e4f91241
Loris Leiva <loris.leiva@gmail.com>
2025-03-29 13:27:03 +0000

    Update @solana/web3.js imports in templates

--- template/shank/clients/js/clients/js/test/_setup.ts
+++ template/shank/clients/js/clients/js/test/_setup.ts
@@ -22,7 +22,7 @@ import {
   setTransactionMessageFeePayerSigner,
   setTransactionMessageLifetimeUsingBlockhash,
   signTransactionMessageWithSigners,
-} from '@solana/web3.js';
+} from '@solana/kit';
 import { findCounterPda, getCreateInstructionAsync } from '../src';
 
 type Client = {
